7fce05f2b04b4904c4eeefc0730114a1cf57580c
Daniel <dandeng24@live.com>
2019-02-26 15:06:23 +1300

    Update TemplateGradeboook.py

--- TemplateGradeboook.py
+++ TemplateGradeboook.py
@@ -4,40 +4,55 @@
 #Standard: 91903, result E
 #Standard: 91905, result E
 
-class Result:
-#When a student sits a standard, they get get a result
-def __init__(self, standard, grade):
-
-#def point_value(self):
-#Used for calculating Grade Score for University entrance, as below
-#N - 0, A - 2, M - 3, E - 4
+def point_value(self, grade, credits):
+    if grade == "E":
+        return credits*4
+    elif grade == "M":
+        return credits*3
+    elif grade =="A":
+        return credits*2
+    elif grade =="NA":
+        return credits*1
 
 def display(self):
-print("Standard: %s, result %s" % (self.standard.standard_number, self.grade))
+    print("Standard: %s, result %s" % (self.standard.standard_number, self.grade))
 
 class Standard:
+    def __init__(self, title, standard_number, version, credit_value):
+        self.title = title
+        self.standard_number = standard_number
+        self.version  = version
+        self.credit = credit_value
 #NZQA standards
 
 class Student():
-#Individual student, incorporating their NZQA results
-def __init__(self, given_name, family_name): 
+    #Individual student, incorporating their NZQA results
+    def __init__(self, given_name, family_name):
+        self.given_name = given_name
+        self.family_name = family_name
 
-def display(self):
-print ("Given name %s, Family name %s" % (self.given_name, self.family_name))
-for result in self.results:
-result.display()
-#Also calculate their total Grade point score and display it
+    def display(self):
+        print ("Given name %s, Family name %s" % (self.given_name, self.family_name))
+        for result in self.results:
+            result.display()
+    #Also calculate their total Grade point score and display it
+
+    #When a student sits a standard, they get get a result
+    def results(self, standard, grade):
+        self.standard = standard
+        self.grade = grade
 
 #create arrays we could append test data
 students = [ ]
 standards = [ ]
+results = [ ]
 
 #append test data to student
 students.append(Student("John", "Martins"))
 #append test data to standards
 standards.append(Standard("Programming", 91906, 6, 1))
 #append data to results, note how indexing 
-students[0].results.append(Result(standards[0], "E"))
+students[0].results.append(result(standards[0], "E"))
 # display data
 for student in students:
-student.display()
+    student.display()
