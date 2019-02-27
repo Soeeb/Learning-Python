3a50f08c2d8a1d6113fb9e900a894f282882fd56
Daniel <dandeng24@live.com>
2019-02-28 10:22:55 +1300

    Update Guess_Game.py

--- Guess_Game.py
+++ Guess_Game.py
@@ -1,3 +1,4 @@
+
 import random
 class Guess_game:
     def __init__(self, player_name):
@@ -7,16 +8,31 @@ class Guess_game:
         self.player_name = player_name
 
     def increment_goes(self):
-        pass
+        self.guesses_so_far += 1
+        if self.guesses_so_far >= 3:
+            self.game_over = True
         
     def check_game_over(self, the_input):
         #Check whether game has finished
-        pass
+        if the_input == self.comp_num:
+            print("You have guessed correctly!")
+            self.game_over = True
+        elif the_input < self.comp_num:
+            print("Higher!!")
+        elif the_input > self.comp_num:
+            print("Lower!")
+        
+ 
+
+
 
 player = input("What's your name?")
 this_game = Guess_game(player)
 
 while not this_game.game_over:
+    this_game.check_game_over(int(input("Please Choose A number!")))
+    this_game.increment_goes()
+
     '''Main loop for the game'''
     #get guess for this turn
     #increment number of goes
@@ -24,4 +40,3 @@ while not this_game.game_over:
     #check whether game over - either they got it right, or they've had 3 incorrect guesses. Call check_game_over()
 
 #display correct message depending on whether they won or lost
-
