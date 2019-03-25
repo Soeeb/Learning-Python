e49447c386259cc7f666b36adc0abc383512eb4c
Daniel <dandeng24@live.com>
2019-03-25 13:08:59 +1300

    savage chicken

---
+++ SavageChicken/SavageChicken/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+class Image:
+        def __init__(self, filename, title):
+            self.filename = filename
+            self.title = title  
+images = []
+import os
+path = os.getcwd()+"/static/images"
+for filename in os.listdir(path):
+    images.append(Image(filename, "Savage Chickens, Doug Savage"))
+
+@app.route('/')
+def main():
+    
+    return render_template("/index.html", images = images)
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT, debug = True)
